1b22eaf37d9c8bddd330871217f63ebe75613574
Sai <saithihak2@gmail.com>
2023-10-10 13:33:11 +0630

    Verify Page

---
+++ src/page-containers/user/auth/verify/index.tsx
@@ -0,0 +1,27 @@
+'use client';
+import React from 'react';
+import Image from 'next/Image';
+
+import { Button } from '@/components/Button/index';
+import { Text } from '@/components/Typo/Text';
+import teeUpLogo from '@/configs/img/auth/teeUpLogo.png';
+
+const Verify = () => {
+  return (
+    <div className="h-screen flex flex-col relative px-5">
+      <div className="flex flex-col justify-evenly h-full items-center w-full flex-1">
+        <Image src={teeUpLogo} width={130} height={31} alt="teeUpLogo" />
+        <form className="mx-auto flex flex-col justify-center gap-3 w-[90%]">
+          <Text className="text-black text-center">
+            We will not be able to save your progress if you do not verify your email. Are you sure
+            you want to proceed without verification?
+          </Text>
+          <Button>Verify Now</Button>
+          <Text className="text-primary text-center">Verify Later</Text>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Verify;
