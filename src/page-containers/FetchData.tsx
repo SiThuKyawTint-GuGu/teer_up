92f0d5e2c21b4f49f8acf970e7aa4e8dc6cd2b0a
KyawNyeinNaing <thetpai.dev@gmail.com>
2023-10-10 11:29:40 +0700

    create: zustand provider

--- src/page-containers/FetchData.tsx
+++ src/page-containers/FetchData.tsx
@@ -1,21 +1,32 @@
 'use client';
 import React, { useCallback, useEffect } from 'react';
 
+import { Button } from '@/components/Button';
 import { FetchAPI } from '@/libs/api';
+import { useStore } from '@/libs/store';
 
 const FetchData = () => {
   const fetchApi = new FetchAPI();
+  const count = useStore(state => state.count);
+  const increment = useStore(state => state.increment);
+  const decrease = useStore(state => state.decrement);
+  console.log(count);
 
   const fetchData = useCallback(async () => {
     const people = await fetchApi.getPeoples('people', {});
-
     console.log(people);
   }, []);
 
   useEffect(() => {
     fetchData();
   }, [fetchData]);
-  return <div>fetchData</div>;
+  return (
+    <div>
+      count: {count}
+      <Button onClick={increment}>Increase</Button>
+      <Button onClick={decrease}>Decrement</Button>
+    </div>
+  );
 };
 
 export default FetchData;
