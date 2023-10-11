8da09662e16141effdc22bf8bbcce1615fee2ef0
KyawNyeinNaing <thetpai.dev@gmail.com>
2023-10-11 16:46:00 +0700

    modified: middleware for protected routes and cookies for server component

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,35 @@
+import { NextURL } from 'next/dist/server/web/next-url';
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 const protectedRoutes = ['/admin', '/dashboard'];
 
 export function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
-  if (protectedRoutes.some(prefix => pathname.startsWith(prefix))) {
-    return NextResponse.rewrite(new URL('/admin/auth/login', req.url));
+  const { pathname } = req.nextUrl as NextURL;
+  const requestHeaders = new Headers(req.headers);
+
+  // example header set
+  // TODO
+  requestHeaders.set('x-hello-from-middleware1', 'hello');
+
+  // Define login URL
+  const loginPath = '/admin/auth/login';
+  const loginUrl = new URL(loginPath, req.url);
+
+  // Get the user's token from cookies
+  const token = req.cookies.get('token')?.value;
+
+  // If the user is not logged in, redirect to the login page
+  if (!token) {
+    return NextResponse.rewrite(loginUrl);
   }
+
+  // If the user is already logged in and tries to access protected routes, redirect to the admin dashboard
+  if (protectedRoutes.some(prefix => pathname.startsWith(prefix))) {
+    return NextResponse.redirect(new URL('/admin', req.nextUrl));
+  }
+
+  // If the user is logged in and accessing other routes, continue processing the request
 }
 
 export const config = {
