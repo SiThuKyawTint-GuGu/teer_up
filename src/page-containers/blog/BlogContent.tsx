659e772440736711ca8b7434b3bf15ce4d08f55f
KyawNyeinNaing <thetpai.dev@gmail.com>
2023-10-16 14:49:07 +0700

    updated: blog ui

--- src/page-containers/blog/BlogContent.tsx
+++ src/page-containers/blog/BlogContent.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import CardBox from '@/components/ui/Card';
 import { Text } from '@/components/ui/Typo/Text';
 import { useGetBlogBySlug } from '@/services/blog';
 import { BlogResponse } from '@/types/Blog';
@@ -8,22 +9,23 @@ import { useParams } from 'next/navigation';
 
 const BlogContent: React.FC = () => {
   const { slug } = useParams();
-
   const { data: blogData } = useGetBlogBySlug<BlogResponse>(slug.toString());
 
-  console.log(blogData);
-
   return (
-    <div className="grid grid-cols-9">
-      <div className="col-start-4 col-span-3">
-        <div className="m-5">
-          <Heading as="h1" size="6" weight="bold">
-            {blogData?.data?.name}
-          </Heading>
-          <Text dangerouslySetInnerHTML={{ __html: blogData?.data?.name || '' }} />
+    <>
+      <div className="bg-[#f3f3f3] h-screen">
+        <div className="grid grid-cols-9">
+          <div className="col-start-4 col-span-3">
+            <CardBox className="bg-white m-5 p-5">
+              <Heading as="h1" size="6" weight="bold">
+                {blogData?.data?.name}
+              </Heading>
+              <Text dangerouslySetInnerHTML={{ __html: blogData?.data?.name || '' }} />
+            </CardBox>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
