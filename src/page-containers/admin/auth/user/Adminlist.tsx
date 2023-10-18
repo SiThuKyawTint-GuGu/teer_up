70e360ecd30dc9b18017a8bfa75ea680d9c7e843
KyawNyeinNaing <thetpai.dev@gmail.com>
2023-10-18 19:33:47 +0700

    bugfix: clear type errors

--- src/page-containers/admin/auth/user/Adminlist.tsx
+++ src/page-containers/admin/auth/user/Adminlist.tsx
@@ -1,17 +1,13 @@
 "use client";
-import Table from "@/components/ui/Table";
+import AdminTable from "@/components/ui/Table";
 import { Box } from "@radix-ui/themes";
 
 const AdminList: React.FC = () => {
-  // const { data: userData } = useGetUser<ParamsType, UserResponse>({
-  //   role: ROLES.ADMIN,
-  // });
-
   return (
     <>
       <Box className="bg-white p-10 rounded-md">
         {/* {userData?.data && <UserTable tableColumns={UserColumns} tableData={userData?.data} />} */}
-        <Table />
+        <AdminTable />
       </Box>
     </>
   );
--- src/page-containers/admin/auth/user/UserList.tsx
+++ src/page-containers/admin/auth/user/UserList.tsx
@@ -1,20 +1,13 @@
 "use client";
 import UserTable from "@/components/ui/Table/UserTable";
-import { ParamsType, useGetUser } from "@/services/user";
-import { ROLES } from "@/shared/enums";
-import { UserResponse } from "@/types/User";
 import { Box } from "@radix-ui/themes";
-import { UserColumns } from "./UserColumns";
 
 const UserList = () => {
-  const { data: userData } = useGetUser<ParamsType, UserResponse>({
-    role: ROLES.USER,
-  });
-
   return (
     <>
       <Box className="bg-white p-10 rounded-md">
-        {userData?.data && <UserTable tableColumns={UserColumns} tableData={userData?.data} />}
+        {/* {userData?.data && <UserTable tableColumns={UserColumns} tableData={userData?.data} />} */}
+        <UserTable />
       </Box>
     </>
   );
