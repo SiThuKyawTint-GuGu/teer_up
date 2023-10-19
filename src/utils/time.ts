978ab44e80f1b00e4d8f359292f08c6a03ae879e
Sai <saithihak2@gmail.com>
2023-10-19 11:19:34 +0630

    feature:comment and like,disable loading

---
+++ src/utils/time.ts
@@ -0,0 +1,21 @@
+import dayjs from "dayjs";
+
+export const showTimeDifference = (createdTime: string) => {
+  const createTime = dayjs(createdTime); // Replace with your actual created-time
+
+  // Get the current time
+  const currentTime = dayjs();
+  const timeDifference = currentTime.diff(createTime);
+
+  // Extract the days, hours, minutes, and seconds from the duration
+  const seconds = Math.floor(timeDifference / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  return {
+    seconds,
+    minutes,
+    hours,
+    days,
+  };
+};
