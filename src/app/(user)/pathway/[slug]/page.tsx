5a3983c82ee51a627b721c9218ee1a0568b9b91f
Sai <saithihak2@gmail.com>
2023-10-24 08:15:12 +0630

    hotfix/navbar-header

--- src/app/(user)/pathway/[slug]/page.tsx
+++ src/app/(user)/pathway/[slug]/page.tsx
@@ -1,7 +1,14 @@
+import ContentDetailHeader from "@/components/contentLayout/ContentDetailHeader";
 import PathwayDetail from "@/page-containers/user/content/components/PathwayDetail";
+import { Grid } from "@radix-ui/themes";
 
 const PathwayPage = () => {
-  return <PathwayDetail />;
+  <Grid columns="1">
+    <div className="absolute  w-full left-0 top-0 z-[9999] flex flex-wrap">
+      <ContentDetailHeader pathname="/home" title="Article Detail" />
+    </div>
+    <PathwayDetail />
+  </Grid>;
 };
 
 export default PathwayPage;
