856e5136ce5de214c64b7a103ad203fc58b7878f
KyawNyeinNaing <thetpai.dev@gmail.com>
2023-10-26 16:49:23 +0700

    modified: saved content

--- src/components/ui/Typo/Text.tsx
+++ src/components/ui/Typo/Text.tsx
@@ -1,6 +1,7 @@
 "use client";
 import * as React from "react";
 
+import { cn } from "@/utils/cn";
 import { Text as RText, textPropDefs } from "@radix-ui/themes";
 
 type SizePropValues = (typeof textPropDefs)["size"]["values"][number];
@@ -24,7 +25,7 @@ export const Text = React.forwardRef<HTMLDivElement, Props>(
     <RText
       ref={ref}
       as={as || "p"}
-      className={className}
+      className={cn("text-black", className)}
       dangerouslySetInnerHTML={dangerouslySetInnerHTML}
       {...(rest as any)}
     >
