da8ed51107b58a3ff80abee55a91a4a8ab2c87c8
Sai <saithihak2@gmail.com>
2023-10-27 16:55:10 +0630

    mentor detail header change

--- src/app/(user)/mentor/[slug]/page.tsx
+++ src/app/(user)/mentor/[slug]/page.tsx
@@ -5,7 +5,7 @@ const Mentor = () => {
   return (
     <>
       <div className="absolute  w-full left-0 top-0 z-[9999] flex flex-wrap">
-        <ContentDetailHeader pathname="/home" title="Event Detail" />
+        <ContentDetailHeader pathname="/home" title="Mentor Detail" />
       </div>
       <MentorProfile />
     </>
