f29a2ae2a9498febb78ffe72adfbb65539f5217c
viabells-khinpyaesone <147359605+viabells-khinpyaesone@users.noreply.github.com>
2023-10-31 17:28:28 +0630

    update ui

--- src/page-containers/admin/form/formDetailConfig.tsx
+++ src/page-containers/admin/form/formDetailConfig.tsx
@@ -1,13 +1,13 @@
 "use client";
 import { useGetFormConfigById, usePostFormConfig, useUpdateFormConfig } from "@/services/formConfig";
 import { useGetInputConfig, usePostInputConfig, useUpdateInputConfig } from "@/services/inputConfig";
-import "@/styles/switch.css";
+// import "@/styles/switch.css";
 import "@/styles/tab.css";
 import { InputConfigResponse } from "@/types/InputConfig";
 import { yupResolver } from "@hookform/resolvers/yup";
 import SaveIcon from "@mui/icons-material/Save";
 import LoadingButton from "@mui/lab/LoadingButton";
-import { Box, InputLabel, TextField } from "@mui/material";
+import { Box, IconButton, InputLabel, Switch, TextField } from "@mui/material";
 import Button from "@mui/material/Button";
 import Checkbox from "@mui/material/Checkbox";
 import FormControl from "@mui/material/FormControl";
@@ -22,13 +22,16 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import * as Switch from "@radix-ui/react-switch";
+// import * as Switch from "@radix-ui/react-switch";
+import AddIcon from "@mui/icons-material/Add";
+import BorderColorIcon from "@mui/icons-material/BorderColor";
+import DeleteIcon from "@mui/icons-material/Delete";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import { useRouter } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 import { useForm } from "react-hook-form";
-import { AiFillDelete, AiTwotoneEdit } from "react-icons/ai";
-import { MdKeyboardArrowUp, MdOutlineKeyboardArrowDown } from "react-icons/md";
 import * as yup from "yup";
 
 interface InputConfigFieldsProps {
@@ -236,7 +239,8 @@ const FormDetailConfigPage = ({ id }: Props) => {
     setEditFormFields(updatedFields);
   };
 
-  const handleSwitchChange = (index: number, isChecked: boolean) => {
+  const handleSwitchChange = (index: number, event: React.ChangeEvent<HTMLInputElement>) => {
+    const isChecked = event.target.checked;
     const updatedFields = editFormFields.map((field, i) => (i === index ? { ...field, required: isChecked } : field));
     setEditFormFields(updatedFields);
   };
@@ -316,7 +320,7 @@ const FormDetailConfigPage = ({ id }: Props) => {
         {/* {error && <p className="text-red-600 mb-2">{error}</p>} */}
         <div className="flex justify-between">
           <div className=" flex-1 border border-gray-300 p-1 py-3 m-1 rounded-md">
-            <p>Input Config</p>
+            <h1 className="m-3 text-lg font-semibold">Input Config</h1>
 
             {fields?.data &&
               fields?.data.length > 0 &&
@@ -337,11 +341,16 @@ const FormDetailConfigPage = ({ id }: Props) => {
                           className="w-full"
                           variant="outlined"
                         />
-                        <AiTwotoneEdit
+                        {/* <AiTwotoneEdit
                           onClick={() => handleUpdateInputConfig(field)}
                           className="ml-2 cursor-pointer text-red-700"
                           size={25}
-                        />
+                        /> */}
+                        <div className="ml-2">
+                          <IconButton onClick={() => handleUpdateInputConfig(field)} color="error">
+                            <BorderColorIcon fontSize="inherit" />
+                          </IconButton>
+                        </div>
                       </div>
                       <Button
                         color="error"
@@ -380,11 +389,15 @@ const FormDetailConfigPage = ({ id }: Props) => {
                         >
                           ADD
                         </Button>
-                        <AiTwotoneEdit
+                        {/* <AiTwotoneEdit
                           onClick={() => handleUpdateInputConfig(field)}
                           className="ml-2 cursor-pointer text-red-700"
                           size={25}
-                        />
+                        /> */}
+
+                        <IconButton onClick={() => handleUpdateInputConfig(field)} color="error">
+                          <BorderColorIcon fontSize="inherit" />
+                        </IconButton>
                       </div>
                     </div>
                   )}
@@ -418,11 +431,16 @@ const FormDetailConfigPage = ({ id }: Props) => {
                         >
                           ADD
                         </Button>
-                        <AiTwotoneEdit
+                        {/* <AiTwotoneEdit
                           onClick={() => handleUpdateInputConfig(field)}
                           className="ml-2 cursor-pointer text-red-700"
                           size={25}
-                        />
+                        /> */}
+                        <div className="ml-2">
+                          <IconButton onClick={() => handleUpdateInputConfig(field)} color="error">
+                            <BorderColorIcon fontSize="inherit" />
+                          </IconButton>
+                        </div>
                       </div>
                     </div>
                   )}
@@ -434,11 +452,16 @@ const FormDetailConfigPage = ({ id }: Props) => {
                           <FormControlLabel control={<Checkbox defaultChecked />} label={field.placeholder} />
                         </FormGroup>
 
-                        <AiTwotoneEdit
+                        {/* <AiTwotoneEdit
                           onClick={() => handleUpdateInputConfig(field)}
                           className="ml-2 cursor-pointer text-red-700"
                           size={25}
-                        />
+                        /> */}
+                        <div className="ml-2">
+                          <IconButton onClick={() => handleUpdateInputConfig(field)} color="error">
+                            <BorderColorIcon fontSize="inherit" />
+                          </IconButton>
+                        </div>
                       </div>
                       <Button
                         color="error"
@@ -463,11 +486,16 @@ const FormDetailConfigPage = ({ id }: Props) => {
                             </LocalizationProvider>
                           </div>
 
-                          <AiTwotoneEdit
+                          {/* <AiTwotoneEdit
                             onClick={() => handleUpdateInputConfig(field)}
                             className="ml-2 cursor-pointer text-red-700"
                             size={25}
-                          />
+                          /> */}
+                          <div className="ml-2">
+                            <IconButton onClick={() => handleUpdateInputConfig(field)} color="error">
+                              <BorderColorIcon fontSize="inherit" />
+                            </IconButton>
+                          </div>
                         </div>
                         <Button
                           color="error"
@@ -486,7 +514,7 @@ const FormDetailConfigPage = ({ id }: Props) => {
             <div className="flex justify-between">
               <div></div>
               <Button onClick={() => setShowModal(true)} color="error" variant="contained" sx={{ width: "50px" }}>
-                +
+                <AddIcon />
               </Button>
             </div>
             <div className="flex justify-between">
@@ -495,12 +523,10 @@ const FormDetailConfigPage = ({ id }: Props) => {
                 {/* Dialog Box */}
                 <Box sx={style}>
                   <div className="mb-3">
-                    <h1>Input Config</h1>
+                    <h1 className="text-xl font-semibold mb-7">Input Config</h1>
                   </div>
                   {error && <p className="text-red-600">{error}</p>}
-                  <div className="mb-5">
-                    <p className="font-weight-600 mb-3">Name*</p>
-
+                  <div className="mb-10">
                     <TextField
                       label="Name"
                       defaultValue={inputConfigName || ""}
@@ -510,9 +536,7 @@ const FormDetailConfigPage = ({ id }: Props) => {
                     />
                   </div>
 
-                  <div className="mb-5">
-                    <p className="font-weight-600 mb-3">Placeholder*</p>
-
+                  <div className="mb-10">
                     <TextField
                       label="Placeholder"
                       defaultValue={inputConfigPlaceholder || ""}
@@ -522,8 +546,6 @@ const FormDetailConfigPage = ({ id }: Props) => {
                     />
                   </div>
                   <div className="mb-10">
-                    <p className="mb-5">Type*</p>
-
                     <FormControl fullWidth>
                       <InputLabel id="selectType-label">Type</InputLabel>
                       <Select
@@ -549,36 +571,33 @@ const FormDetailConfigPage = ({ id }: Props) => {
                       <p>Options</p>
                       {options.map((field, index) => (
                         <>
-                          <div key={index} className="flex justify-evenly p-5 rounded-md border border-gray-300 my-2">
+                          <div
+                            key={index}
+                            className="flex justify-between items-center p-5 rounded-md border border-gray-300 my-2"
+                          >
                             <div className="mb-2 mr-2">
-                              <p className="font-weight-600 mb-3">Label*</p>
-                              <fieldset className="Fieldset mb-10">
-                                <input
-                                  className="Input"
-                                  name="label"
-                                  defaultValue={field.label}
-                                  onChange={e => handleOptionChange(e, field.value)}
-                                />
-                              </fieldset>
+                              <TextField
+                                name="label"
+                                onChange={e => handleOptionChange(e, field.value)}
+                                defaultValue={field.label}
+                                label="Label"
+                                variant="outlined"
+                              />
                             </div>
                             <div className="mb-2">
-                              <p className="font-weight-600 mb-3">Value*</p>
-                              <fieldset className="Fieldset mb-10">
-                                <input
-                                  className="Input"
-                                  name="value"
-                                  defaultValue={field.value}
-                                  onChange={e => handleOptionChange(e, field.value)}
-                                />
-                              </fieldset>
-                            </div>
-                            <button onClick={() => handleDeleteOption(field.value)}>
-                              <AiFillDelete
-                                // onClick={() => setShowModal(true)}
-                                size={20}
-                                className="text-red-500 cursor-pointer"
+                              <TextField
+                                name="value"
+                                onChange={e => handleOptionChange(e, field.value)}
+                                defaultValue={field.value}
+                                label="Value"
+                                variant="outlined"
                               />
-                            </button>
+                            </div>
+                            <div>
+                              <IconButton onClick={() => handleDeleteOption(field.value)} color="error" size="large">
+                                <DeleteIcon fontSize="inherit" />
+                              </IconButton>
+                            </div>
                           </div>
                         </>
                       ))}
@@ -632,7 +651,7 @@ const FormDetailConfigPage = ({ id }: Props) => {
 
           {/* Edit form config */}
           <div className=" flex-1 border border-gray-300 p-1 py-3 m-1 rounded-md">
-            <p>Edit Form Config</p>
+            <h1 className="m-3 text-lg font-semibold">Edit Form Config</h1>
             {editFormFields &&
               editFormFields.length > 0 &&
               editFormFields.map((field, index) => (
@@ -745,43 +764,40 @@ const FormDetailConfigPage = ({ id }: Props) => {
                     <div className=" mt-3 flex items-center justify-between">
                       <form>
                         <div style={{ display: "flex", alignItems: "center" }}>
-                          <label className="Label mr-1" htmlFor="airplane-mode">
-                            Is Required
-                          </label>
-                          <Switch.Root
-                            className="SwitchRoot mr-3"
-                            id="airplane-mode"
-                            checked={field.required}
-                            // onChange={}
-                            onCheckedChange={isChecked => handleSwitchChange(index, isChecked)}
-                          >
-                            <Switch.Thumb className="SwitchThumb" />
-                          </Switch.Root>
+                          <FormControlLabel
+                            control={
+                              <Switch
+                                checked={field.required}
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                                  handleSwitchChange(index, event)
+                                }
+                                color="error"
+                              />
+                            }
+                            sx={{ color: "gray" }}
+                            label="Required"
+                          />
                         </div>
                       </form>
                       <div className="flex justify-center">
-                        <div
-                          // disabled={index === 0}
-                          onClick={index === 0 ? undefined : () => handleMoveField(index, index - 1)}
+                        <IconButton
+                          disabled={index === 0}
+                          onClick={() => handleMoveField(index, index - 1)}
+                          color="error"
                         >
-                          <MdKeyboardArrowUp size={25} className="text-gray-700 hover:text-red-700 cursor-pointer" />
-                        </div>
-                        <div
-                          // disabled={index === editFormFields.length - 1}
-                          // onClick={() => handleMoveField(index, index + 1)}
-                          onClick={
-                            index === editFormFields.length - 1 ? undefined : () => handleMoveField(index, index + 1)
-                          }
+                          <KeyboardArrowUpIcon fontSize="inherit" />
+                        </IconButton>
+                        <IconButton
+                          disabled={index === editFormFields.length - 1}
+                          onClick={() => handleMoveField(index, index + 1)}
+                          color="error"
                         >
-                          <MdOutlineKeyboardArrowDown
-                            size={25}
-                            className="text-gray-700 hover:text-red-700 cursor-pointer"
-                          />
-                        </div>
+                          <KeyboardArrowDownIcon fontSize="inherit" />
+                        </IconButton>
                       </div>
-                      <button onClick={() => handleDeleteField(field.order)}>
-                        <AiFillDelete size={20} className="text-red-500 cursor-pointer" />
-                      </button>
+                      <IconButton onClick={() => handleDeleteField(field.order)} color="error">
+                        <DeleteIcon fontSize="inherit" />
+                      </IconButton>
                     </div>
                   </div>
                 </>
@@ -790,7 +806,7 @@ const FormDetailConfigPage = ({ id }: Props) => {
 
           {/* Preview */}
           <div className="flex-1 border border-gray-300 p-1 py-3 m-1 rounded-md">
-            <p>Preview</p>
+            <h1 className="m-3 text-lg font-semibold">Preview</h1>
             <div className=" flex flex-col pb-10 m-3 rounded-md">
               {editFormFields.length > 0 &&
                 editFormFields.map((field, index) => (
