1fe286aed536dd63d0d45c060fa6f64f6fd9c13e
KyawNyeinNaing <thetpai.dev@gmail.com>
2023-11-08 18:29:47 +0700

    modified: updated font types

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -4,8 +4,24 @@ import localFont from "next/font/local";
 export const lFont = localFont({
   src: [
     {
-      path: "../fonts/FSAlbertPro-Light.ttf",
+      path: "../fonts/FSAlbertPro-Thin.ttf",
       weight: "300",
+    },
+    {
+      path: "../fonts/FSAlbertPro-Light.ttf",
+      weight: "400",
+    },
+    {
+      path: "../fonts/FSAlbertPro.ttf",
+      weight: "500",
+    },
+    {
+      path: "../fonts/FSAlbertPro-Bold.ttf",
+      weight: "700",
+    },
+    {
+      path: "../fonts/FSAlbertPro-ExtraBold.ttf",
+      weight: "800",
     },
   ],
   variable: "--font-teeup",
