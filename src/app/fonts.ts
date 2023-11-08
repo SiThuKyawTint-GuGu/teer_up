3ae38fd57bb1a170290ddda09db7b9d6f7f77a2a
KyawNyeinNaing <thetpai.dev@gmail.com>
2023-11-08 18:20:40 +0700

    feat: implement font family

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,4 +1,15 @@
-import { Inter as FontSans, JetBrains_Mono as FontMono } from "next/font/google";
+import { JetBrains_Mono as FontMono, Inter as FontSans } from "next/font/google";
+import localFont from "next/font/local";
+
+export const lFont = localFont({
+  src: [
+    {
+      path: "../fonts/FSAlbertPro-Light.ttf",
+      weight: "300",
+    },
+  ],
+  variable: "--font-teeup",
+});
 
 export const fontSans = FontSans({
   subsets: ["latin"],
