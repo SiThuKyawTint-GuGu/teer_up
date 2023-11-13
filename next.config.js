79c09e755e95a2df04d47047cdc004f592af6a4c
Sai <saithihak2@gmail.com>
2023-11-13 17:46:09 +0630

    scroll top remove

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     domains: ["teeup-dev.s3.amazonaws.com", "teeup-dev.s3.ap-southeast-1.amazonaws.com", "karawanico.com"],
   },
   experimental: {
+    appDir: true,
     scrollRestoration: true,
   },
 };
