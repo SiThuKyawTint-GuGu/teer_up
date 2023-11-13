927c1f6cb7e0fbe2d9861bf52dabf7cc817b1cb5
KyawNyeinNaing <thetpai.dev@gmail.com>
2023-11-13 14:52:44 +0700

    modified: edit education

--- src/page-containers/user/profile/EditEducation.tsx
+++ src/page-containers/user/profile/EditEducation.tsx
@@ -59,6 +59,7 @@ const EditEducation: React.FC = () => {
       { edu_id: edu_id as string },
       {
         onSuccess: () => {
+          console.log("deleted");
           router.push(`/profile/${id}/education`);
         },
       }
