ca0750eb8f73c51bec2d690a72e2a2863227a5e5
KyawNyeinNaing <thetpai.dev@gmail.com>
2023-11-14 15:03:49 +0700

    bugfix: exp delete error

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -37,11 +37,13 @@ const Layout = ({ children }: Props) => {
         return response;
       },
       function (error) {
-        if (error.response.status == 401) {
-          verifyModalOpenHandler();
-        }
-        if (error.response.status == 403) {
-          verifyEmailModalOpenHandler();
+        if (error?.response) {
+          if (error.response.status == 401) {
+            verifyModalOpenHandler();
+          }
+          if (error.response.status == 403) {
+            verifyEmailModalOpenHandler();
+          }
         }
         return Promise.reject(error);
       }
