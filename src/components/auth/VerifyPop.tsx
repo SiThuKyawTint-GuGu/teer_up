2dc7a5834008425e1694d81146a390e8576e6b55
KyawNyeinNaing <thetpai.dev@gmail.com>
2023-11-15 17:55:09 +0700

    modified: verify login in dialog

--- src/components/auth/VerifyPop.tsx
+++ src/components/auth/VerifyPop.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import { useVerifyModal } from "@/store/authStore";
-import { Flex } from "@radix-ui/themes";
+import { Box, Flex } from "@radix-ui/themes";
 import { useRouter } from "next/navigation";
 import React, { useTransition } from "react";
 import { Button } from "../ui/Button";
-import { Icons } from "../ui/Images";
-import Modal from "../ui/Modal";
+import { Dialog, DialogContent } from "../ui/Dialog";
 import { Text } from "../ui/Typo/Text";
 
 const VerifyPop: React.FC = () => {
@@ -16,37 +15,29 @@ const VerifyPop: React.FC = () => {
 
   return (
     <>
-      {openVerifyModal && (
-        <Modal onClose={() => verifyModalCloseHandler()}>
-          <Flex
-            className="px-[16px] bg-white py-[40px] w-full  max-w-[400px]"
-            position="relative"
-            direction="column"
-            gap="5"
-          >
-            <Text as="div" className="w-full text-center text-[36px] font-[700]">
-              You&apos;re not logged in
-            </Text>
-            <Text as="div" className="w-full text-center">
-              Log in now to comment and save the contents for better experience
-            </Text>
-            <Button
-              size="lg"
-              disabled={isPending}
-              className="mb-5"
-              onClick={() => {
-                verifyModalCloseHandler();
-                router.push("/auth/login");
-              }}
-            >
-              Login
-            </Button>
-            <div className="absolute top-3 right-3" onClick={verifyModalCloseHandler}>
-              <Icons.closeCircle className="w-[24px] h-[24px] text-slateGray" />
-            </div>
-          </Flex>
-        </Modal>
-      )}
+      <Dialog open={openVerifyModal}>
+        <DialogContent isClose={true} closeStyles="top-[30px] right-[30px]" handleOnClose={verifyModalCloseHandler}>
+          <Box width="100%" className="bg-white rounded-md px-4 py-6">
+            <Flex position="relative" direction="column" gap="5" className="space-y-4">
+              <Text className="w-full text-center text-[34px] font-[700]">You&apos;re not logged in</Text>
+              <Text className="w-full text-center">
+                Log in now to comment and save the contents for better experience
+              </Text>
+              <Button
+                disabled={isPending}
+                onClick={() => {
+                  verifyModalCloseHandler();
+                  startTransition(() => {
+                    router.push("/auth/login");
+                  });
+                }}
+              >
+                Login
+              </Button>
+            </Flex>
+          </Box>
+        </DialogContent>
+      </Dialog>
     </>
   );
 };
