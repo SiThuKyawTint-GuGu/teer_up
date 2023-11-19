27e82dfdffc02d9af339abf8fc8d53347703e976
KyawNyeinNaing <thetpai.dev@gmail.com>
2023-11-19 11:19:11 +0700

    feat: link copied dialog box

--- src/components/contentLayout/ReactionBar.tsx
+++ src/components/contentLayout/ReactionBar.tsx
@@ -1,12 +1,14 @@
 import CommentSection from "@/components/contentLayout/CommentSection";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/Dialog";
-import { Icons } from "@/components/ui/Images";
+import { Icons, Image } from "@/components/ui/Images";
 import { useLikeContent, useSaveContent } from "@/services/content";
 import { ContentData } from "@/types/Content";
 import { getToken } from "@/utils/auth";
 import { cn } from "@/utils/cn";
+import { Box, Flex } from "@radix-ui/themes";
 import React, { useEffect, useState } from "react";
 import Share from "../../page-containers/admin/content/Share";
+import { Text } from "../ui/Typo/Text";
 
 enum dialogTrigger {
   COMMENT = "comment",
@@ -69,7 +71,7 @@ const ReactionBar: React.FC<ReactionBarProp> = ({ data, contentMutate, comments,
 
   return (
     <>
-      <Dialog>
+      <Dialog onOpenChange={val => !val && setIsCopied(!isCopied)}>
         <div className="w-full py-2">
           <div className="flex justify-between items-center">
             <button className="flex items-center flex-wrap gap-x-[10px]" onClick={likePost}>
@@ -129,18 +131,25 @@ const ReactionBar: React.FC<ReactionBarProp> = ({ data, contentMutate, comments,
               </DialogContent> */}
           </div>
         </div>
-        <DialogContent
-          className={cn(
-            "bg-white top-[initial] bottom-0 px-0 py-2 translate-y-0 rounded-16px-tl-tr",
-            isCopied && "top-0 bg-red"
-          )}
-        >
+        <DialogContent className={cn("bg-white top-[initial] bottom-0 px-0 py-2 translate-y-0 rounded-16px-tl-tr")}>
           {triggerType === dialogTrigger.COMMENT ? (
             <CommentSection data={data} mutateParentData={contentMutate} setComments={setComments} />
           ) : (
             <Share url={`/content/${data.slug}`} onClickCopied={setIsCopied} />
           )}
         </DialogContent>
+        {isCopied && (
+          <DialogContent className="border-0 shadow-none outline-none">
+            <Flex justify="center">
+              <Box className="w-[180px] px-6 py-3 rounded-full bg-white">
+                <Flex justify="center" align="center" className="gap-x-2">
+                  <Image src="/uploads/icons/checkmark.svg" width={24} height={24} alt="check" />
+                  <Text className="text-[#373A36] text-lg font-semibold">Link copied</Text>
+                </Flex>
+              </Box>
+            </Flex>
+          </DialogContent>
+        )}
       </Dialog>
     </>
   );
