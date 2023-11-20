47a184033fc8520763c64f0b9fb5cc5fa69441fa
KyawNyeinNaing <thetpai.dev@gmail.com>
2023-11-20 21:04:28 +0700

    Bugfix: height of dialog box being full height

--- src/components/auth/VerifyPop.tsx
+++ src/components/auth/VerifyPop.tsx
@@ -16,10 +16,10 @@ const VerifyPop: React.FC = () => {
   return (
     <>
       <Dialog open={openVerifyModal}>
-        <DialogContent isClose={true} closeStyles="top-[30px] right-[30px]" handleOnClose={verifyModalCloseHandler}>
-          <Box width="100%" className="bg-white rounded-md px-4 py-6">
+        <DialogContent isClose={true} closeStyles="top-3 right-3" handleOnClose={verifyModalCloseHandler}>
+          <Box width="100%" className="bg-white h rounded-md px-4 pb-6 pt-12">
             <Flex position="relative" direction="column" gap="5" className="space-y-4">
-              <Text className="w-full text-center text-[34px] font-[700]">You&apos;re not logged in</Text>
+              <Text className="w-full text-center text-[28px] font-[700]">You&apos;re not logged in</Text>
               <Text className="w-full text-center">
                 Log in now to comment and save the contents for better experience
               </Text>
