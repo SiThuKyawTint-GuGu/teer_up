ecc55c1575760d958dc2229f037fe4736edced2d
Sai <saithihak2@gmail.com>
2023-11-20 10:55:01 +0630

    progress bar for loading more content in Explore

---
+++ src/components/ui/Progress/TailwindProgress.tsx
@@ -0,0 +1,7 @@
+import "./index.css";
+
+const TailwindProgress = () => {
+  return <div className="h-12 w-12 border-t-4 border-blue-500 border-opacity-25 animate-linearMovement"></div>;
+};
+
+export default TailwindProgress;
