7f7e86cabe54347520a7e80d4d40f09a8289bbc3
viabells-winko <140389613+viabells-winko@users.noreply.github.com>
2024-05-06 17:29:40 +0630

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -94,5 +94,12 @@ export const config = {
     "/pathway/:path*",
     "/events/:path*",
     "/",
+    {
+      source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+      missing: [
+        { type: "header", key: "next-router-prefetch" },
+        { type: "header", key: "purpose", value: "prefetch" },
+      ],
+    },
   ],
 };
