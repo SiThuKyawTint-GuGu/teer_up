cf8006f78cb70f6585d08df68d1bd77ee056c879
viabells-winko <140389613+viabells-winko@users.noreply.github.com>
2024-05-06 17:31:56 +0630

    Revert "add: cspHeader"
    This reverts commit 0974e46b3e28c70516df1e937a3de19b0835b230.

--- next.config.js
+++ next.config.js
@@ -1,32 +1,5 @@
 /** @type {import('next').NextConfig} */
-
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-`;
-
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: cspHeader.replace(/\n/g, ""),
-          },
-        ],
-      },
-    ];
-  },
   images: {
     domains: ["teeup-dev.s3.amazonaws.com", "teeup-dev.s3.ap-southeast-1.amazonaws.com", "karawanico.com"],
   },
