dddffd38aa0ef6d4a17f01c52fe150aafbb0d51a
Khant Sithu <khantsithu@viabells.com>
2024-05-07 03:24:48 +0000

    feat: content history

--- src/app/(user)/content-history/page.tsx
+++ src/app/(user)/content-history/page.tsx
@@ -1,3 +1,18 @@
-export default function ContentHistory() {
-  return <div>ContentHistory</div>;
-}
+import MainPageLayout from "@/components/userLayout/MainPageLayout";
+import ContentHistoryPage from "@/page-containers/user/content-history";
+import { NextPage } from "next";
+
+export const metadata = {
+  title: "Content History",
+  description: "Content History Page",
+};
+
+const ContentHistory: NextPage = () => {
+  return (
+    <MainPageLayout>
+      <ContentHistoryPage />
+    </MainPageLayout>
+  );
+};
+
+export default ContentHistory;
