58a1a0ca3e59306ff98a26cd0051461dbe64bf20
Thura <thurasoe@viabells.com>
2024-05-07 22:00:28 +0630

    update sidebar drawer

--- src/shared/data/UserTabbar.tsx
+++ src/shared/data/UserTabbar.tsx
@@ -20,12 +20,6 @@ export const navbarItems: NavbarType[] = [
     activeIcon: <Icons.exploreIcon width={24} height={24} activeFill="#DA291C" active />,
   },
   {
-    text: "Mentorship",
-    path: "/mentorship",
-    icon: <Icons.mentorshipIcon width={24} height={24} />,
-    activeIcon: <Icons.mentorshipIcon width={24} height={24} activeFill="#DA291C" active />,
-  },
-  {
     text: "Saved",
     path: "/saved",
     icon: <Icons.savedIcon width={24} height={24} fill="#373A36" />,
@@ -34,13 +28,25 @@ export const navbarItems: NavbarType[] = [
   {
     text: "Content History",
     path: "/content-history",
-    icon: <Icons.savedIcon width={24} height={24} fill="#373A36" />,
-    activeIcon: <Icons.savedIcon width={24} height={24} activeFill="#DA291C" active />,
+    icon: <Icons.historyIcon width={24} height={24} fill="#373A36" />,
+    activeIcon: <Icons.historyIcon width={24} height={24} activeFill="#DA291C" active />,
   },
   {
-    text: "Profile",
-    path: "/profile",
-    icon: <Icons.profileIcon width={24} height={24} fill="#373A36" />,
-    activeIcon: <Icons.profileIcon width={24} height={24} activeFill="#DA291C" active />,
+    text: "Chat",
+    path: "/chat",
+    icon: <Icons.chatIcon width={24} height={24} />,
+    activeIcon: <Icons.chatIcon width={24} height={24} activeFill="#DA291C" active />,
+  },
+  {
+    text: "Setting",
+    path: "/setting",
+    icon: <Icons.settingIcon width={24} height={24} />,
+    activeIcon: <Icons.settingIcon width={24} height={24} activeFill="#DA291C" active />,
+  },
+  {
+    text: "Mentorship",
+    path: "/mentorship",
+    icon: <Icons.mentorshipIcon width={24} height={24} />,
+    activeIcon: <Icons.mentorshipIcon width={24} height={24} activeFill="#DA291C" active />,
   },
 ];
