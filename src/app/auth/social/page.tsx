e8cbc63e96dcdcf45795d3af2e75ac4b59b9dd1d
Thura <thurasoe@viabells.com>
2024-05-07 12:47:05 +0630

    Add social auth page and social buttons

---
+++ src/app/auth/social/page.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+import SocialAuth from "@/page-containers/user/auth/social";
+
+const SocialPage: NextPage = () => {
+  return <SocialAuth />;
+};
+
+export default SocialPage;
