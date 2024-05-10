691b1ae68f8a5a1762f25cc621592e4aead8832f
Khant Sithu <khantseithuu@gmail.com>
2024-05-10 11:32:37 +0000

    feat: charts for dashboard page

---
+++ src/page-containers/school/dashboard/DashboardPage.tsx
@@ -0,0 +1,44 @@
+"use client"
+import {Container, Box, Card, CardHeader, CardContent, Typography} from "@mui/material";
+import ChartLine from "@/page-containers/school/dashboard/ChartLine";
+
+export default function DashboardPage() {
+  return (
+    <Container sx={{ mb: 10 }}>
+      <Typography variant="h6" fontWeight="bold" my={4}>Dashboard</Typography>
+      <Box
+        gap={3}
+        display="grid"
+        gridTemplateColumns={{ xs: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
+      >
+        <Card>
+          <CardHeader title="Number of Opportunities" />
+          <CardContent>
+            <ChartLine
+              series={[
+                {
+                  name: 'Opportunities',
+                  data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
+                },
+              ]}
+            />
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader title="Number of Successful Applications" />
+          <CardContent>
+            <ChartLine
+              series={[
+                {
+                  name: 'Applications',
+                  data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
+                },
+              ]}
+            />
+          </CardContent>
+        </Card>
+      </Box>
+    </Container>
+  )
+}
