aac45f4f6362526abc7a2656ad739edf67776f72
Khant Sithu <khantsithu@viabells.com>
2024-05-13 05:43:41 +0000

    fixed: icon

--- src/shared/data/SchoolTabbar.tsx
+++ src/shared/data/SchoolTabbar.tsx
@@ -11,8 +11,8 @@ export const navbarItems: NavbarType[] = [
   {
     text: "Dashboard",
     path: "/school/dashboard",
-    icon: <Icons.schoolDashboardIcon width={24} height={24} fill="#373A36" />,
-    activeIcon: <Icons.schoolDashboardIcon width={24} height={24} activeFill="#DA291C" active />,
+    icon: <Icons.schoolBlogIcon width={24} height={24} fill="#373A36" />,
+    activeIcon: <Icons.schoolBlogIcon width={24} height={24} activeFill="#DA291C" active />,
   },
   {
     text: "Opportunities",
@@ -23,13 +23,13 @@ export const navbarItems: NavbarType[] = [
   {
     text: "Students",
     path: "/school/students",
-    icon: <Icons.schoolStudentsIcon width={24} height={24} fill="#373A36" />,
-    activeIcon: <Icons.schoolStudentsIcon width={24} height={24} activeFill="#DA291C" active />,
+    icon: <Icons.schoolBlogIcon width={24} height={24} fill="#373A36" />,
+    activeIcon: <Icons.schoolBlogIcon width={24} height={24} activeFill="#DA291C" active />,
   },
   {
     text: "Setting",
     path: "/school/setting",
-    icon: <Icons.schoolSettingsIcon width={24} height={24} fill="#373A36" />,
-    activeIcon: <Icons.schoolSettingsIcon width={24} height={24} activeFill="#DA291C" active />,
+    icon: <Icons.schoolBlogIcon width={24} height={24} fill="#373A36" />,
+    activeIcon: <Icons.schoolBlogIcon width={24} height={24} activeFill="#DA291C" active />,
   },
 ];
