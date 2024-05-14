e31a6db0ad81bef124fb198145bd0c10b956809a
Khant Sithu <khantsithu@viabells.com>
2024-05-14 05:26:06 +0000

    feat: degree table

--- src/app/admin/schools/[id]/page.tsx
+++ src/app/admin/schools/[id]/page.tsx
@@ -1,13 +1,31 @@
+import DegreeForm from "@/page-containers/admin/schools/degreeForm";
+import DegreeTable from "@/page-containers/admin/schools/degreeTable";
 import SchoolDetails from "@/page-containers/admin/schools/schoolDetails";
+import { Box, Container } from "@mui/material";
 
 interface Props {
   params: { id: string };
 }
 const SchoolDetailPage = ({ params: { id } }: Props) => {
   return (
-    <>
+    <Container
+      component="main"
+      maxWidth="xl"
+      sx={{
+        marginY: 8,
+      }}
+    >
       <SchoolDetails id={id} />
-    </>
+
+      {/* degree */}
+      <Box my={4}>
+        <DegreeTable id={id} />
+      </Box>
+
+      <Box my={4}>
+        <DegreeForm id={id} />
+      </Box>
+    </Container>
   );
 };
 
