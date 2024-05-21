e10f171fc87ccb05b566ac6d36f251d6200ca164
Khant Sithu <khantseithuu@gmail.com>
2024-05-21 13:33:17 +0630

    feat: add EducationLevelPage

---
+++ src/app/admin/schools/education-level/page.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+
+export default function EducationLevelPage() {
+  return (
+    <Box
+      sx={{
+        pt: 4,
+        px: 2,
+        maxWidth: "lg",
+      }}
+    >
+      Education Level
+    </Box>
+  );
+}
