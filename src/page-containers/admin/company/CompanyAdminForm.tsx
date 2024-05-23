5799292bdfeadde38faafbb124fb0577719865d8
Khant Sithu <khantseithuu@gmail.com>
2024-05-23 11:05:18 +0630

    feat: create company admin form

---
+++ src/page-containers/admin/company/CompanyAdminForm.tsx
@@ -0,0 +1,134 @@
+"use client";
+
+import { useCreateUser } from "@/services/user";
+import { USER_ROLE } from "@/shared/enums";
+import { yupResolver } from "@hookform/resolvers/yup";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import LoadingButton from "@mui/lab/LoadingButton";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormHelperText,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  name: yup.string().required("Company Name is required"),
+  email: yup.string().email().required("Email is required"),
+  password: yup.string().required("Password is required").min(8, "Password must be at least 8 characters"),
+});
+
+export default function CompanyAdminForm() {
+  const router = useRouter();
+  const [showPassword, setShowPassword] = useState(false);
+  const { trigger: createUser, isMutating: creatingUser } = useCreateUser();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  const onSubmit = (data: { name: string; email: string; password: string }) => {
+    const submitData = {
+      ...data,
+      role: USER_ROLE.COMPANY,
+    };
+    console.log(submitData);
+    // createUser(submitData, {
+    //   onSuccess: () => {
+    //     router.back();
+    //   },
+    // });
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 4,
+        width: "100%",
+        maxWidth: "600px",
+        m: 2,
+      }}
+      component={"form"}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <Typography variant="h4" fontWeight="bold">
+        Add Company Admin
+      </Typography>
+      <TextField
+        label="Company Name"
+        {...register("name")}
+        error={!!errors.name}
+        helperText={errors.name?.message}
+        fullWidth
+      />
+      <TextField
+        label="Company Email"
+        {...register("email")}
+        error={!!errors.email}
+        helperText={errors.email?.message}
+      />
+
+      <FormControl fullWidth variant="outlined">
+        <InputLabel>Old Password</InputLabel>
+
+        <OutlinedInput
+          label="Password"
+          type={showPassword ? "text" : "password"}
+          {...register("password")}
+          error={!!errors.password}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOffIcon /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+        {errors.password && <FormHelperText error>{errors.password.message}</FormHelperText>}
+      </FormControl>
+      <Box>
+        <LoadingButton type="submit" loading={isSubmitting || creatingUser} variant="contained">
+          Submit
+        </LoadingButton>
+        <Button
+          variant="outlined"
+          sx={{
+            ml: 2,
+          }}
+          onClick={() => router.back()}
+        >
+          Cancel
+        </Button>
+      </Box>
+    </Box>
+  );
+}
