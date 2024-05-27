78314e0f4918ca5b5fbfeb35f8790a6d7cb1443d
Si Thu Kyaw Tint <sithukyawtint@viabells.com>
2024-05-27 09:56:44 +0630

    fix type error

--- src/types/Education.ts
+++ src/types/Education.ts
@@ -1,7 +1,7 @@
 export interface Education {
   degree_id: any;
-  other_school_degree: ReactNode;
-  other_school_name: ReactNode;
+  other_school_degree: any;
+  other_school_name: any;
   school_id: any;
   school: any;
   degree_relation: any;
@@ -15,7 +15,7 @@ export interface Education {
   updated_at: string;
   user_id: string;
   major: any;
-  other_school_major: ReactNode;
+  other_school_major: any;
 }
 
 export interface EducationResponse {
