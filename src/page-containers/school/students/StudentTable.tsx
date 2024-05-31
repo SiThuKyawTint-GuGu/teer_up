f21ce67e912e94d511b527babd33408882a115d7
Khant Sithu <khantsithu@viabells.com>
2024-05-31 03:52:33 +0000

    feat: remove students with checkboxes

--- src/page-containers/school/students/StudentTable.tsx
+++ src/page-containers/school/students/StudentTable.tsx
@@ -1,20 +1,22 @@
 "use client";
 
-import DeleteIcon from "@mui/icons-material/Delete";
-import EditIcon from "@mui/icons-material/Edit";
-import InfoIcon from "@mui/icons-material/Info";
-import LoadingButton from "@mui/lab/LoadingButton";
-import { Box, Button, Container, IconButton, Modal, Tooltip, Typography } from "@mui/material";
-import dayjs from "dayjs";
-import { MRT_ColumnDef, MRT_TableOptions, MaterialReactTable, useMaterialReactTable } from "material-react-table";
-import { useEffect, useMemo, useState } from "react";
-import { Student, StudentGroup, StudentGroupDataResponse } from "@/types/school/StudentGroup";
 import {
   useDeleteStudentGroup,
   useGetStudentGroupById,
-  useGetStudentGroups,
+  useRemoveStudentFromGroup,
   useUpdateStudentGroup,
 } from "@/services/school/studentGroup";
+import { Student, StudentGroup, StudentGroupDataResponse } from "@/types/school/StudentGroup";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { Box, Button, Container, Modal, Typography } from "@mui/material";
+import {
+  MRT_ColumnDef,
+  MRT_RowSelectionState,
+  MRT_TableOptions,
+  MaterialReactTable,
+  useMaterialReactTable,
+} from "material-react-table";
+import { useEffect, useMemo, useState } from "react";
 
 import AddStudentToGroupModal from "@/page-containers/school/students/AddStudentToGroupModal";
 
@@ -44,6 +46,8 @@ export default function StudentTable({ id: studentGroupId }: props) {
     pageSize: 10,
   });
   const [validationErrors, setValidationErrors] = useState<Record<string, string | undefined>>({});
+  const [rowSelection, setRowSelection] = useState<MRT_RowSelectionState>({});
+  const { trigger: removeStudentFromGroup, isMutating: removingStudents } = useRemoveStudentFromGroup();
 
   useEffect(() => {
     if (studentGroupsData) setStudents(studentGroupsData?.data.students);
@@ -77,30 +81,6 @@ export default function StudentTable({ id: studentGroupId }: props) {
         accessorKey: "email",
         header: "Email",
       },
-      {
-        accessorKey: "created_at",
-        header: "Created At",
-        enableEditing: false,
-        Cell: ({ row }: any) => {
-          const createdAt = row.original.created_at;
-          if (createdAt && dayjs(createdAt).isValid()) {
-            return dayjs(createdAt).format("MMM D, YYYY h:mm A");
-          }
-          return "";
-        },
-      },
-      {
-        accessorKey: "updated_at",
-        header: "Updated At",
-        enableEditing: false,
-        Cell: ({ row }: any) => {
-          const updatedAt = row.original.updated_at;
-          if (updatedAt && dayjs(updatedAt).isValid()) {
-            return dayjs(updatedAt).format("MMM D, YYYY h:mm A");
-          }
-          return "";
-        },
-      },
     ],
     [validationErrors]
   );
@@ -120,6 +100,21 @@ export default function StudentTable({ id: studentGroupId }: props) {
     table.setEditingRow(null);
   };
 
+  const removeSelectedStudents = async () => {
+    const selectedRows = table.getSelectedRowModel().rows;
+    const formattedData = {
+      group_id: parseInt(studentGroupId),
+      student_emails: selectedRows.map((row: any) => row.original.email),
+    };
+
+    removeStudentFromGroup(formattedData, {
+      onSuccess: () => {
+        mutate();
+        setRowSelection({});
+      },
+    });
+  };
+
   const table = useMaterialReactTable({
     columns: columns as any,
     data: (students as any) || [],
@@ -135,8 +130,8 @@ export default function StudentTable({ id: studentGroupId }: props) {
       : undefined,
     muiTableContainerProps: {
       sx: {
-        maxHeight: "calc(100vh - 200px)",
-        minHeight: "480px",
+        height: "calc(100vh - 320px)",
+        overflow: "auto",
       },
     },
     positionActionsColumn: "last",
@@ -149,10 +144,13 @@ export default function StudentTable({ id: studentGroupId }: props) {
         pageIndex: 0,
       },
     },
+    enableRowSelection: true,
+    onRowSelectionChange: setRowSelection,
     state: {
       showSkeletons: isLoading ?? false,
       pagination,
       isLoading,
+      rowSelection,
     },
     enableStickyHeader: true,
     enableStickyFooter: true,
@@ -166,39 +164,22 @@ export default function StudentTable({ id: studentGroupId }: props) {
     onCreatingRowCancel: () => setValidationErrors({}),
     onEditingRowCancel: () => setValidationErrors({}),
     onEditingRowSave: handleSave,
-    // renderRowActions: ({ row, table }) => (
-    //   <Box sx={{ display: "flex", gap: "1rem" }}>
-    //     <Tooltip title="Edit">
-    //       {/* <Link href={`/admin/banners/${row.id}`}> */}
-    //       <IconButton onClick={() => table.setEditingRow(row)}>
-    //         <EditIcon />
-    //       </IconButton>
-    //       {/* </Link> */}
-    //     </Tooltip>
-    //     <Tooltip title="Details">
-    //       <Link href={`/school/student-group/${row.id}`}>
-    //         <IconButton>
-    //           <InfoIcon />
-    //         </IconButton>
-    //       </Link>
-    //     </Tooltip>
-    //     <Tooltip title="Delete">
-    //       <IconButton
-    //         color="error"
-    //         onClick={() => {
-    //           setId(row.id);
-    //           setOpen(true);
-    //         }}
-    //       >
-    //         <DeleteIcon />
-    //       </IconButton>
-    //     </Tooltip>
-    //   </Box>
-    // ),
     renderTopToolbarCustomActions: ({ table }) => (
       <Box display="flex" columnGap={2}>
         <AddStudentToGroupModal id={studentGroupId} type={"addstudent"} />
-        <AddStudentToGroupModal id={studentGroupId} type={"removestudent"} />
+        {rowSelection && Object.keys(rowSelection).some(key => rowSelection[key]) && (
+          <LoadingButton
+            onClick={removeSelectedStudents}
+            loading={removingStudents}
+            disabled={!Object.keys(rowSelection).some(key => rowSelection[key])}
+            variant="contained"
+            size="small"
+            color="error"
+            sx={{ textTransform: "none" }}
+          >
+            Remove Selected
+          </LoadingButton>
+        )}
       </Box>
     ),
   });
@@ -266,17 +247,6 @@ export default function StudentTable({ id: studentGroupId }: props) {
   );
 }
 
-const style = {
-  position: "absolute" as "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  boxShadow: 24,
-  p: 4,
-};
-
 function validate(values: StudentGroup) {
   const errors: Record<string, string | undefined> = {};
 
@@ -286,3 +256,15 @@ function validate(values: StudentGroup) {
 
   return errors;
 }
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
